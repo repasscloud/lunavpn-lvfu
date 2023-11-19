9f4f26e2e6d9838af65225c523d90f4b650bdf88
Danijel-James Wynyard <danijel@repasscloud.com>
2023-11-19 19:11:46 +1100

    add replacement to lvfucpp

---
+++ lvfucs/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace lvfucs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfArguments = args.Length;
+
+            switch (numberOfArguments)
+            {
+                case 1:
+                    Console.WriteLine("One argument provided.");
+                    switch (args[0])
+                    {
+                        case "-h":
+                        case "--help":
+                            break;
+                        case "-v":
+                        case "--version":
+                            break;
+                    }
+                    break;
+
+                case 2:
+                    Console.WriteLine("Two arguments provided.");
+                    // Access the arguments with args[0] and args[1]
+                    break;
+
+                case 6:
+                    Console.WriteLine("Six arguments provided.");
+                    // Access the arguments with args[0], args[1], ..., args[5]
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid number of arguments.");
+                    break;
+            }
+        }
+    }
+}
